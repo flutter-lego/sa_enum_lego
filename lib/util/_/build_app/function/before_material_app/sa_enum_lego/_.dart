6a21264294cb9f5d89e86f6c1aa6a5ad7041b6e8
june lee <melodysdreamj@gmail.com>
2024-06-01 21:25:19 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/sa_enum_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForSaEnumLego(BuildContext context) async {}
---
+++ lib/util/_/build_app/widget/run_app/sa_enum_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverRunApp()
+// Widget coverSaEnumLego(Widget widget) {
+//   return widget;
+// }
